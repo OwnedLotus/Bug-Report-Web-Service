13e46a61f5c898f04d0ee71b5cbd86dd3db44420
Jonah Ringdahl <46978061+OwnedLotus@users.noreply.github.com>
2021-11-29 14:23:10 -0500

    Authorization
    added authorization

--- BugReportService/Controllers/ReportsController.cs
+++ BugReportService/Controllers/ReportsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BugReportService.Data;
 using BugReportService.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BugReportService.Controllers
 {
@@ -44,6 +45,7 @@ namespace BugReportService.Controllers
         }
 
         // GET: Reports/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -54,6 +56,7 @@ namespace BugReportService.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Create([Bind("Id,Title,Description,IsResolved")] Report report)
         {
             if (ModelState.IsValid)
@@ -66,6 +69,7 @@ namespace BugReportService.Controllers
         }
 
         // GET: Reports/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -86,6 +90,7 @@ namespace BugReportService.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,IsResolved")] Report report)
         {
             if (id != report.Id)
@@ -117,6 +122,7 @@ namespace BugReportService.Controllers
         }
 
         // GET: Reports/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -137,6 +143,7 @@ namespace BugReportService.Controllers
         // POST: Reports/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var report = await _context.Report.FindAsync(id);
@@ -145,6 +152,7 @@ namespace BugReportService.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        [Authorize]
         private bool ReportExists(int id)
         {
             return _context.Report.Any(e => e.Id == id);
