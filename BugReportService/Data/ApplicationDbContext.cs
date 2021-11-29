22b36828f431d2f05e1c1d6ffef9b0eb584a79da
Jonah Ringdahl <Jonahringdahl@outlook.com>
2021-11-28 20:42:07 -0500

    Initial Commit
    Including the database, report model, and the View and controller

---
+++ BugReportService/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BugReportService.Models;
+
+namespace BugReportService.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BugReportService.Models.Report> Report { get; set; }
+    }
+}
---
+++ BugReportService/Models/Report.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugReportService.Models
+{
+    public class Report
+    {
+        private readonly DateTime _dateEntered = DateTime.Now;
+        private static int _id = 1;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = GenerateId(); }
+        }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public bool IsResolved { get; set; }
+        public DateTime DateEntered 
+        { 
+            get { return _dateEntered; } 
+        }
+
+        public Report()
+        {
+
+        }
+
+        public Report(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        /// <summary>
+        /// Auto Increment the id for the Bug.Id
+        /// </summary>
+        /// <returns>
+        /// Incremented Id
+        /// </returns>
+        static int GenerateId()
+        {
+            return _id++;
+        }
+
+    }
+}
